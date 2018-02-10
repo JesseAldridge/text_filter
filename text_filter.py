9ddebfecb7d08b961325ffe95a6f0e8037000cb6
Jesse Aldridge <JesseAldridge@gmail.com>
2018-02-09 23:18:14 -0800

    print mutators on no arg

--- text_filter.py
+++ text_filter.py
@@ -1,27 +1,40 @@
 #!/usr/bin/python
-import sys, importlib, os
+import sys, importlib, os, glob
 
 import clipboard
 
-filters_path = '/Users/jesse_aldridge/Dropbox/text_filter/filters'
-sys.path.append(filters_path)
+def main():
+  filters_path = os.path.expanduser('~/Dropbox/text_filter/filters')
+  sys.path.append(filters_path)
 
-testing = (len(sys.argv) == 1 or 'test' in sys.argv)
-mutator_name = 'normal_words' if testing else sys.argv[1]
-module = importlib.import_module(mutator_name)
-mutator = getattr(module, mutator_name)
+  if len(sys.argv) == 1:
+    print 'mutators:'
+    for path in glob.glob(filters_path + '/*.py'):
+      filename = os.path.basename(path).rsplit('.py')[0]
+      if filename == '__init__':
+        continue
+      print '  ' + filename
+    return
 
-def each_line(text, mutator):
-  in_lines = text.splitlines()
-  clean_lines = []
-  for line in in_lines:
-    clean_lines.append(mutator(line))
-  return '\n'.join([line for line in clean_lines if line.strip()])
+  testing = 'test' in sys.argv
+  mutator_name = 'normal_words' if testing else sys.argv[1]
+  module = importlib.import_module(mutator_name)
+  mutator = getattr(module, mutator_name)
 
-if testing:
-  with open('test.txt') as f:
-    text = f.read()
-else:
-  text = clipboard.paste()
+  def each_line(text, mutator):
+    in_lines = text.splitlines()
+    clean_lines = []
+    for line in in_lines:
+      clean_lines.append(mutator(line))
+    return '\n'.join([line for line in clean_lines if line.strip()])
 
-print each_line(text, mutator)
+  if testing:
+    with open('test.txt') as f:
+      text = f.read()
+  else:
+    text = clipboard.paste()
+
+  print each_line(text, mutator)
+
+if __name__ == '__main__':
+  main()
